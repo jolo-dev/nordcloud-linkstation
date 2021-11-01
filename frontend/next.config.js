7ba874fafc51a8f642fdde21dabbc3e8668baa1a
jolo-dev <johnyscrazy@gmail.com>
2021-11-01 21:35:01 +0000

    Make gitpod and next.js working

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   async rewrites() {
+    console.log("test");
     return [
       {
-        source: '/api',
-        destination: 'http://localhost:8000/best-powerstation',
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/:path*',
       },
     ]
   },
